4b3afd7ac3a77896fa0e62fead8165b873271382
k17-vp-1752004 <1752004@student.hcmus.edu.vn>
2019-08-11 09:57:37 +0700

    modifier ficher scripts

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,32 +1,28 @@
 function validateRegister() {
     var email = document.getElementById("txtemail");
     var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-    if (!re.test(email.value))                               
-    {
+    if (!re.test(email.value)) {
         window.alert("Please enter your valid email."); 
         email.focus(); 
         return false; 
     } 
     var phonenumber = document.getElementById("txtphonenumber");
     re = /^[0-9]{10,11}$/;
-    if (!re.test(phonenumber.value))
-    {
+    if (!re.test(phonenumber.value)) {
         window.alert("Please enter your valid phone number");
         phonenumber.focus();
         return false;
     }
     var pwd = document.getElementById("txtpassword");
     re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-    if (!re.test(pwd.value))
-    {
+    if (!re.test(pwd.value)) {
         window.alert("Your password must have at least 8 characters and contain at least one lowercase, one uppercase and one digit");
         pwd.focus();
         return false;
     }
 
     var confirmpwd = document.getElementById("txtconfirmpassword");
-    if (pwd.value != confirmpwd.value)
-    {
+    if (pwd.value != confirmpwd.value) {
         window.alert("Password mismatch");
         confirmpwd.focus();
         return false;
@@ -38,8 +34,7 @@ function validateRegister() {
     const textInput = document.getElementById('album_id');
     const artistName = textInput.value;
     url = "/register";
-    fetch(url,
-    {
+    fetch(url, {
         method: 'POST',
         headers: {
             'Content-type': 'application/json; charset=UTF-8' ,
@@ -57,16 +52,39 @@ function validateRegister() {
     event.preventDefault();
     return false;
 }
+
+function setQuantity(ops) {
+    var quantity = document.getElementById("quantity");
+    if (ops == 'up') {
+        quantity.value++;
+    }
+    else if (ops == 'down') {
+        if (quantity.value > 1) {
+            quantity.value--;
+        }
+    }
+}
+
 function onResponse(response){
-    console.log(response);
     return response.json();
 }
+
 function onJsonReady(json){
     alert(json.message);
-    // window.location.href('/');
-
-    // if (json.Success) {
-    //     window.location.href('/');
-    // }
     return true;
+}
+
+async function postComment(event, productId) {
+    console.log(productId);
+    return false;
+    event.preventDefault();
+    var username = document.getElementById("input-name").value;
+    var commentContent = document.getElementById("txt-editor").value;
+    if (!username) {
+        return false;
+    }
+    if (!commentContent) {
+        return false;
+    }
+    var productId = document.getElementById
 }
